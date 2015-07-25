8faa87b7fc1875b0705a149dedcd6f6ccce16b8a
draconicti <hockeymanalaska24@yahoo.com>
2015-07-24 17:33:53 -0800

    logo fix

--- extDB-Build/Bornholm_Life.Bornholm/dialog/ui.hpp
+++ extDB-Build/Bornholm_Life.Bornholm/dialog/ui.hpp
@@ -15,7 +15,7 @@
 			class logo : life_RscPicture 
 			{
 			idc = -1;
-			text = "textures\crftlogo.png";
+			text = "textures\crftlogo.paa";
 			x = safeZoneX+safeZoneW-0.22; y = safeZoneY+safeZoneH-0.28;
 			w = 0.2; h = 0.29;
 			};
